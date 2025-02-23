1a0c56bc3b8893f19397071a685ad1c17374d530
Valter Vieira Gomes Junior <tekraseg02@gmail.com>
2025-02-23 01:10:04 -0300

    atualização das dependências

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-  basePath: '',
 };
+
+module.exports = nextConfig;
