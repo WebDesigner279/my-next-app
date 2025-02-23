3d32157d79988e32e8d4af7a6b1847387045f4ac
Valter Vieira Gomes Junior <tekraseg02@gmail.com>
2025-02-23 02:18:31 -0300

    correção de erro 404

--- next.config.js
+++ next.config.js
@@ -1,8 +1,32 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   experimental: {
+//     appDir: true,
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
+  },
+  images: {
+    domains: [], // Adicione os domínios de imagens se necessário
+  },
+  env: {
+    API_URL: process.env.API_URL, // Variáveis de ambiente, configure na Vercel
+  },
+  sassOptions: {
+    includePaths: [require('path').join(__dirname, 'styles')],
+  },
+  async rewrites() {
+    return [
+      { source: '/(.*)', destination: '/' }, // Redireciona todas as rotas para a página inicial
+    ];
   },
 };
 
