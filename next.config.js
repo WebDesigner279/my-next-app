3acd3d3badedfb7249fbdce278319298c88a8b6b
Valter Vieira Gomes Junior <tekraseg02@gmail.com>
2025-02-23 01:00:16 -0300

    atualização next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   reactStrictMode: true,
   experimental: {
-    appDir: true, // Ativa o App Router
+    appDir: true,
   },
+  basePath: '',
 };
-
-module.exports = nextConfig;
